b612ac4acf5d453185b86418b250711ff0c06667
Arshia Alaie <arshiaalaie1@gmail.com>
2022-08-18 10:43:02 +0430

    initiate django project

---
+++ backend/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def the_ultimate_function(request):
+    data = request.POST
+    grid = data.get('grid')
+    ships = data.get('ships')
+    
+    return HttpResponse('hello')
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', the_ultimate_function)
+]
